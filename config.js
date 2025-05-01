27c116293834f4e662a0f38733a12a74efe49a45
Anuga123 <anjanee.uragaha@gmail.com>
2025-05-01 18:42:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //Put Session-id Here
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "", //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "", //Put Your Github Auth Token
-DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || "SADIYA-MD-DATABASE", //Bot Database Repo Name
+SESSION_ID: process.env.SESSION_ID || "", //SADIYA-MD=mZkV0bZT#d54MuszuneGo-YWYXb8XfRVxoD8nHxfYsFG1k_Rr59w
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "", //Anuga123
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "", //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
+DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || "SADIYA-MD-DATABASE", //SADIYA-MD
 SESSION_NAME: process.env.SESSION_NAME || "session"
 };
